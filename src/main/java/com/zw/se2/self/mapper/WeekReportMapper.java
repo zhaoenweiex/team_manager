2597820dcd71d80ad4e895e48013d011e18dead8
zhaoenwei <zhaoenwei@foxmail.com>
2017-08-08 23:44:03 +0800

    完成周報展示相關功能

--- src/main/java/com/zw/se2/self/mapper/WeekReportMapper.java
+++ src/main/java/com/zw/se2/self/mapper/WeekReportMapper.java
@@ -1,9 +1,8 @@
 package com.zw.se2.self.mapper;
 
 import com.zw.se2.self.model.WeekReport;
-import org.apache.ibatis.annotations.Insert;
-import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.*;
+import org.apache.ibatis.type.JdbcType;
 
 import java.util.List;
 
@@ -12,11 +11,17 @@ import java.util.List;
  */
 @Mapper
 public interface WeekReportMapper {
-    @Select( "select * from week_report")
+    @Select( "select done_info,plan_info,problem_info from week_report where 1=1")
+    @Results(value = {
+            @Result(id = true, property = "id", column = "id", javaType = Integer.class, jdbcType = JdbcType.BIGINT),
+            @Result(property = "doneInfo", column = "done_info", javaType = String.class, jdbcType = JdbcType.VARCHAR),
+            @Result(property = "planInfo", column = "plan_info", javaType = String.class, jdbcType = JdbcType.VARCHAR),
+            @Result(property = "problemInfo", column = "problem_info", javaType = String.class, jdbcType = JdbcType.VARCHAR)
+            })
     List<WeekReport> findAll();
-    @Insert("insert into week_report(create_time,user_id,done_info,plan_info,problem_info,project_info) values(#{userId},#{createTime},#{doneInfo},#{planInfo},#{problemInfo},#{projectInfo})")
+    @Insert("insert into week_report(create_time,user_id,done_info,plan_info,problem_info,project_info) values(#{createTime},#{userId},#{doneInfo},#{planInfo},#{problemInfo},#{projectInfo})")
     Boolean create(WeekReport weekReport);
-    @Select("select * from week_report where (user_id) = (#{userId})")
+    @Select("select * from week_report where user_id = #{userId}")
     List<WeekReport> findAllByUserId(Integer userId);
     @Select("select * from week_report where id in (#{ids})")
     List<WeekReport> findInIds(String[] ids);
