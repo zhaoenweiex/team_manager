c3912c734f3669e9c9dc89d7776801c276d2aaa5
zhaoenwei <zhaoenwei@foxmail.com>
2017-08-10 23:16:51 +0800

    mvp版0.5

---
+++ ext/sql/team_manager_week_report.sql
@@ -0,0 +1,3 @@
+INSERT INTO team_manager.week_report (create_time, done_info, plan_info, project_info, off_work_info, business_out_info, user_id, problem_info, org_id, overtime_info) VALUES (null, '123fsdggs', '23213', 'asdas', null, null, null, 'asdasd', null, null);
+INSERT INTO team_manager.week_report (create_time, done_info, plan_info, project_info, off_work_info, business_out_info, user_id, problem_info, org_id, overtime_info) VALUES ('1502206576871', 'dota', '高達', '啊實打實大蘇打;S大蘇打撒旦;asd', null, null, 1, 'els', null, null);
+INSERT INTO team_manager.week_report (create_time, done_info, plan_info, project_info, off_work_info, business_out_info, user_id, problem_info, org_id, overtime_info) VALUES ('1502206579326', 'dota', '高達', '啊實打實大蘇打;S大蘇打撒旦;asd', null, null, 1, 'els', null, null);
