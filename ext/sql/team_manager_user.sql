52700e9431445113f56c1bf2464499e216370597
zhaoenwei <zhaoenwei@foxmail.com>
2017-08-29 23:57:53 +0800

    sql

---
+++ ext/sql/team_manager_user.sql
@@ -0,0 +1,2 @@
+INSERT INTO team_manager.user (name, password, type, orgId) VALUES ('admin', 'admin', 'admin', '1');
+INSERT INTO team_manager.user (name, password, type, orgId) VALUES ('zew', 'zew', 'member', '1');
